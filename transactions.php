0df191775bdec4160326d6c0349c6e787aac8f1d
Spangsberg Jørgensen <smsj@MBPtilhangsberg.local>
2021-10-24 22:59:06 +0200

    Update docs and comments etc.

--- transactions.php
+++ transactions.php
@@ -10,27 +10,33 @@
 
     try {
 
-        // $conn->beginTransaction();
+        //$conn->beginTransaction();
 
         // withdraw 1000 from savings account
         $handle = $conn->prepare("UPDATE BankAccount SET Balance = Balance - 1000 WHERE AccountID = 1");
         $handle->execute();
 
-        // throw new Exception("Simulate error....");
+        //throw new Exception("Simulate error....");
 
         // transfer 1000 to the spendingsaccount
         $handle = $conn->prepare("UPDATE BankAccount SET Balance = Balance + 1000 WHERE AccountID = 2");
         $handle->execute();
 
-        // $conn->commit();
+        // $conn->commit(); // commit both queries as ONE atomic unit (ACID principles...)
         // echo "Committed transaction.<br/>";
         echo "Succesfully transferred your money :)<br/>";
     }
     catch (Exception $e) {
         echo "An error occured: " . $e . "<br/>";
 
-        //$conn->rollBack();
-       // echo "Rollback.... Nothing has been committed.";  
+        // $conn->rollBack(); // if we experience an error, explicitly rollback the first query, so we still have a consistent db (will probably be handled by PDO anyways though)
+
+        /* From http://www.php.net/manual/en/pdo.transactions.php: 
+        When the script ends or when a connection is about to be closed, if you have an outstanding transaction, PDO will automatically roll it back. ... if you didn't explicitly commit the transaction, then it is assumed that something went awry, so the rollback is performed for the safety of your data. 
+        
+        Nevertheless, it is a good practice to explicitly rollback a transaction in case of error. */ 
+
+        // echo "Rollback.... Nothing has been committed.";  
     }
 
 ?>
